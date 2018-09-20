5f7f30ce5694b3ece598b2e965e1b053f65da2a8
eden <xiaoyumqyz@gmail.com>
2018-09-20 17:07:53 +1200

    add capitalize util

--- projects/common/src/lib/common.ts
+++ projects/common/src/lib/common.ts
@@ -59,4 +59,16 @@ export class CommonUtil {
     }
     return 'Unkonwn';
   }
+
+
+  /* Convert the first character to upper case and the remaining to lower case.
+ * |Name  |Type  |Desc                |
+ * |------|------|--------------------|
+ * |str   |string|String to capitalize|
+ * |return|string|Capitalized string  |
+ * capitalize('rED'); // -> Red
+ */
+  static capitalized(str) {
+    return str.charAt(0).toUpperCase() + str.substring(1).toLowerCase();
+  }
 }
