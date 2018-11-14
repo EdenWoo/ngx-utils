939aaea4d04196194dd3efac35f5e9135aa5f642
eden <xiapyumqyz@gmail.com>
2018-11-14 15:52:47 +1300

    new version published

--- projects/iw-core/src/lib/abstract-value-accessor.ts
+++ projects/iw-core/src/lib/abstract-value-accessor.ts
@@ -2,42 +2,43 @@ import {forwardRef} from '@angular/core';
 import {ControlValueAccessor, NG_VALUE_ACCESSOR} from '@angular/forms';
 
 export abstract class AbstractValueAccessor implements ControlValueAccessor {
-    _value: any = '';
-    get value(): any {
-        return this._value;
-    };
+  _value: any = '';
+  get value(): any {
+    return this._value;
+  };
 
-    set value(v: any) {
-        if (v !== this._value) {
-            this._value = v;
-            this.onChange(v);
-        }
+  set value(v: any) {
+    if (v !== this._value) {
+      this._value = v;
+      this.onChange(v);
     }
+  }
 
-    writeValue(value: any) {
-        this._value = value;
-        // warning: comment below if only want to emit on user intervention
-        this.onChange(value);
-    }
+  writeValue(value: any) {
+    this._value = value;
+    // warning: comment below if only want to emit on user intervention
+    this.onChange(value);
+  }
 
-    onChange = (_) => {
-    };
-    onTouched = () => {
-    };
+  onChange = (_) => {
+  }
+  onTouched = () => {
+  }
 
-    registerOnChange(fn: (_: any) => void): void {
-        this.onChange = fn;
-    }
+  registerOnChange(fn: (_: any) => void): void {
+    this.onChange = fn;
+  }
 
-    registerOnTouched(fn: () => void): void {
-        this.onTouched = fn;
-    }
+  registerOnTouched(fn: () => void): void {
+    this.onTouched = fn;
+  }
 }
 
-export function MakeProvider(type: any) {
-    return {
-        provide: NG_VALUE_ACCESSOR,
-        useExisting: forwardRef(() => type),
-        multi: true
-    };
+// https://stackoverflow.com/questions/43335336/error-ts4058-return-type-of-exported-function-has-or-is-using-name-x-from-exter
+export function MakeProvider(type: any): any {
+  return {
+    provide: NG_VALUE_ACCESSOR,
+    useExisting: forwardRef(() => type),
+    multi: true
+  };
 }
