a2c63f11a742db3ddfb6687fbaa0042bf85b1648
eden <xiapyumqyz@gmail.com>
2018-11-15 18:01:51 +1300

    add dropzone and ckeditor

--- projects/iw-core/src/lib/iw-core.module.ts
+++ projects/iw-core/src/lib/iw-core.module.ts
@@ -5,23 +5,30 @@ import {NgSelectModule} from '@ng-select/ng-select';
 import {CommonModule} from '@angular/common';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
-
+import {IwDropzoneComponent} from './iw-dropzone/iw-dropzone.component';
+import {DropzoneModule} from 'ngx-dropzone-wrapper';
+import {IwCkeditorComponent} from './iw-ckeditor/iw-ckeditor.component';
 @NgModule({
-  imports: [
-    NgSelectModule,
-    CommonModule,
-    ReactiveFormsModule,
-    FormsModule,
-    HttpClientModule
-  ],
-  declarations: [
-    IwCoreComponent,
-    IwNgSelectComponent
-  ],
-  exports: [
-    IwCoreComponent,
-    IwNgSelectComponent
-  ]
+    imports: [
+        NgSelectModule,
+        CommonModule,
+        ReactiveFormsModule,
+        FormsModule,
+        HttpClientModule,
+        DropzoneModule
+    ],
+    declarations: [
+        IwCoreComponent,
+        IwNgSelectComponent,
+        IwDropzoneComponent,
+        IwCkeditorComponent
+    ],
+    exports: [
+        IwCoreComponent,
+        IwNgSelectComponent,
+        IwDropzoneComponent,
+        IwCkeditorComponent
+    ]
 })
 export class IwCoreModule {
 }
