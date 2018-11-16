281aedaf459c2ebf0158f555d35a1d2240841a5c
eden <xiapyumqyz@gmail.com>
2018-11-16 16:42:06 +1300

    new version published

--- projects/iw-core/src/public_api.ts
+++ projects/iw-core/src/public_api.ts
@@ -6,3 +6,4 @@ export * from './lib/iw-core.service';
 export * from './lib/iw-core.component';
 export * from './lib/iw-core.module';
 export * from './lib/services/iw-authentication.service';
+export * from './lib/base/form-base-component/iw-form-base.component';
