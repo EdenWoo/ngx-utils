327b750c182998a5b5ca0ea7351a89b3b9739711
eden <xiapyumqyz@gmail.com>
2018-11-17 23:35:51 +1300

    add base

---
+++ projects/iw-base/src/public_api.ts
@@ -0,0 +1,12 @@
+/*
+ * Public API Surface of iw-base
+ */
+
+export * from './lib/services/iw-authentication.service';
+export * from './lib/environments/environment';
+export * from './lib/base/form-base-component/iw-form-base.component';
+export * from './lib/app/constants/app.constant';
+export * from './lib/app/constants/enums.constant';
+export * from './lib/app/constants/project.constant';
+export * from './lib/app/constants/permission.constant';
+
